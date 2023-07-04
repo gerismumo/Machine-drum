4b23148e5e73de94de7eaf1e0335d39259867732
gerald <geraldmumo6@gmail.com>
2023-07-04 17:30:29 +0300

    text display commit

--- file.js
+++ file.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
     var drumPad = $('.drum-pad');
+    var displayAudioId = $('#display');
 
     drumPad.click( function() {
         var drumPadId = $(this).attr('id');
 
         var audio = $("#" + drumPadId + ".clip");
         audio.get(0).play(); 
-    })
+
+        displayAudioId.text(drumPadId.toString() + " " + "is playing").css("color" , "red");
+    });
+
+    
+
 });
